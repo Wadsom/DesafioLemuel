01a9aa04823313b7f494baacb2e84a17bfccf3a5
Wadson <pinheirowadson@gmail.com>
2024-09-27 17:06:21 -0300

    feat: Criando Entidades Motorcycle e User na camada de Domain

---
+++ dsMotor/Entities/Motorcycle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dsMotor.Domain.Entities
+{
+    public class Motorcycle
+    {
+        public int Id { get; private set; }
+        public string  Model { get; private set; }
+        public int Age { get; private set; }
+        public string  Plate { get; private set; }
+        public string  Color { get; private set; }
+
+        public Motorcycle(string model, int age, string plate, string color)
+        {
+            Model = model;
+            Age = age;
+            Plate = plate;
+            Color = color;
+        }
+    }
+}
---
+++ dsMotor/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dsMotor.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string  Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+}
